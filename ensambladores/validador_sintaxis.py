953e2bdb9ac3c73bc923926df5dfdfbab3ade995
Alejandro2236 <alejandro.1701814320@ucaldas.edu.co>
2024-11-19 23:17:41 -0500

    Implementación de las funciones "separar_palabras", "validar_codop", "obtener_nombres_codop", "validar_cantidad_operandos" y "obtener_cantidad_operandos_codop" en validador_sintaxis.py

--- ensambladores/validador_sintaxis.py
+++ ensambladores/validador_sintaxis.py
@@ -13,14 +13,34 @@ class ValidadorSintaxis:
         palabras = self.__separar_palabras(instruccion)
         codop: str = palabras[0]
         operandos: list[str] = palabras[1:]
+
         self.__validar_codop(codop)
         self.__validar_cantidad_operandos(codop, operandos)
 
     def __separar_palabras(self, instruccion: str) -> list[str]:
-        raise NotImplementedError
+        if not instruccion:
+            raise ValueError("Para separar una instrucción en palabras no puede estar vacia")
+        return instruccion.split()
 
     def __validar_codop(self, codop: str):
-        raise NotImplementedError
+        if codop not in self.__obtener_nombres_codop():
+            raise ValueError(f"Código de operación '{codop}' desconocido")
+
+    def __obtener_nombres_codop(self) -> list[str]:
+        return [codop["nombre"] for codop in self.__codops_info]
 
     def __validar_cantidad_operandos(self, codop: str, operandos: list[str]):
-        raise NotImplementedError
+        cantidad_operandos_esperados: int = self.__obtener_cantidad_operandos_codop(codop)
+
+        if len(operandos) != cantidad_operandos_esperados:
+            raise ValueError(
+                f"La instrucción con código de operación '{codop}' espera {cantidad_operandos_esperados}"
+                f" operandos, pero se proporcionaron {len(operandos)}."
+            )
+
+    def __obtener_cantidad_operandos_codop(self, codop: str) -> int:
+        for codop_info in self.__codops_info:
+            if codop_info["nombre"] == codop:
+                return codop_info["operandos"]
+
+        raise ValueError(f"Código de operación '{codop}' desconocido")

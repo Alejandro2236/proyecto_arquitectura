f41e48ea96258edd4206cfe1a310316969ba5afb
Alejandro2236 <alejandro.1701814320@ucaldas.edu.co>
2024-11-19 22:47:55 -0500

    Creación de la estructura principal para validar instrucción en validador_sintaxis.py

--- ensambladores/validador_sintaxis.py
+++ ensambladores/validador_sintaxis.py
@@ -10,4 +10,17 @@ class ValidadorSintaxis:
         return True
 
     def __validar_instruccion(self, instruccion: str):
+        palabras = self.__separar_palabras(instruccion)
+        codop: str = palabras[0]
+        operandos: list[str] = palabras[1:]
+        self.__validar_codop(codop)
+        self.__validar_cantidad_operandos(codop, operandos)
+
+    def __separar_palabras(self, instruccion: str) -> list[str]:
+        raise NotImplementedError
+
+    def __validar_codop(self, codop: str):
+        raise NotImplementedError
+
+    def __validar_cantidad_operandos(self, codop: str, operandos: list[str]):
         raise NotImplementedError

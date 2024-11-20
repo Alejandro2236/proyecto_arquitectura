0b41714b1a4aca34c84c208e8d20d404f8cf525b
Alejandro2236 <alejandro.1701814320@ucaldas.edu.co>
2024-11-19 22:44:24 -0500

    Adición de información en los codops de unidad_control.py y creación de la estructura principal para validar programa en validador_sintaxis.py

--- ensambladores/validador_sintaxis.py
+++ ensambladores/validador_sintaxis.py
@@ -1,3 +1,13 @@
 class ValidadorSintaxis:
-    def __init__(self, codops: dict):
-        self.__codops = list(codops.values())
+    def __init__(self, codops_info: dict):
+        self.__codops_info: list[dict] = list(codops_info.values())
+
+    def validar_programa(self, instrucciones: list[str]) -> bool:
+        if not instrucciones:
+            raise ValueError("El programa debe tener al menos una instrucción.")
+        for instruccion in instrucciones:
+            self.__validar_instruccion(instruccion)
+        return True
+
+    def __validar_instruccion(self, instruccion: str):
+        raise NotImplementedError
--- modelos/unidad_control.py
+++ modelos/unidad_control.py
@@ -1,9 +1,9 @@
 class UnidadControl:
     __CODOPS: dict = {
-        "00000": "ADD",
-        "00001": "SUB",
-        "00010": "MUL",
-        "00011": "DIV",
+        "00000": {"nombre": "ADD", "cantidad_operandos": 3},
+        "00001": {"nombre": "SUB", "cantidad_operandos": 3},
+        "00010": {"nombre": "MUL", "cantidad_operandos": 3},
+        "00011": {"nombre": "DIV", "cantidad_operandos": 3},
     }
 
     @property

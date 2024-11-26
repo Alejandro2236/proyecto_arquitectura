776eb58dae1d8118cb684e20e38bac3359ed004a
Alejandro2236 <alejandro.1701814320@ucaldas.edu.co>
2024-11-26 10:22:02 -0500

    Implementacion de obtener_info_codops y separación de validar_unidad_control en controlador_unidad_control.py

--- controladores/controlador_unidad_control.py
+++ controladores/controlador_unidad_control.py
@@ -12,13 +12,21 @@ class ControladorUnidadControl:
         self.__unidad_control: UnidadControl = UnidadControl()
 
     def obtener_nombres_codop(self) -> set[str]:
-        if self.__unidad_control is None:
-            raise ValueError("La unidad de control no ha sido inicializada.")
-
+        self.__validar_unidad_control()
         self.__inicializar_dict_codops()
         nombres_codops: set = {info["nombre"] for info in self.__dict_codops}
         return nombres_codops
 
-    def __inicializar_dict_codops(self):
+    def obtener_info_codops(self) -> list[dict]:
+        self.__validar_unidad_control()
+        self.__inicializar_dict_codops()
+        info_codops = list(self.__dict_codops.values())
+        return info_codops
+
+    def __validar_unidad_control(self):
+        if self.__unidad_control is None:
+            raise ValueError("La unidad de control no ha sido inicializada.")
+
+    def __inicializar_dict_codops(self) -> None:
         if self.__dict_codops is None:
             self.__dict_codops: dict = self.__unidad_control.codops

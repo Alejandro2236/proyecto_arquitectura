3901aaa02d4cd2c5cdbe657eb4f53820237120c9
Alejandro2236 <alejandro.1701814320@ucaldas.edu.co>
2024-12-08 17:14:45 -0500

    Creación de envio de dirección a mar y métodos de acceso en mar.py

--- modelos/unidad_control_cableada.py
+++ modelos/unidad_control_cableada.py
@@ -1,8 +1,13 @@
+from modelos.mar import Mar
+
+
 class UnidadControlCableada:
     def __init__(self, diccionario_componentes: dict):
         self.__componentes: dict = diccionario_componentes
 
-
-
     def mover_valor(self, origen: str, destino: str):
         ...
+
+    def enviar_direccion_a_mar(self, direccion: str):
+        mar: Mar = self.__componentes["mar"]
+        mar.registro = direccion

96fedbbd643e7479b681e8228d129f81312dff54
Alejandro2236 <alejandro.1701814320@ucaldas.edu.co>
2024-12-08 16:41:16 -0500

    Creación de unidad_control_cableada.py

---
+++ modelos/unidad_control_cableada.py
@@ -0,0 +1,8 @@
+class UnidadControlCableada:
+    def __init__(self, diccionario_componentes: dict):
+        self.__componentes: dict = diccionario_componentes
+
+
+
+    def mover_valor(self, origen: str, destino: str):
+        ...

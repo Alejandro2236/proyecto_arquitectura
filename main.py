0d9b0df30a1481332574c329044ecf8ecf132400
Alejandro2236 <alejandro.1701814320@ucaldas.edu.co>
2024-12-10 02:30:59 -0500

    Configuración inicial en main

--- main.py
+++ main.py
@@ -3,14 +3,21 @@ from threading import Thread
 import time
 
 from modelos.alu import ALU
+from modelos.bus_control import BusControl
+from modelos.bus_datos import BusDatos
+from modelos.bus_direcciones import BusDirecciones
+from modelos.memoria_datos import MemoriaDatos
+from modelos.memoria_instrucciones import MemoriaInstrucciones
 from modelos.monitor import DataMonitor
 from modelos.psw import Psw
 from modelos.ir import Ir
 from modelos.mar import Mar
 from modelos.mbr import Mbr
 from modelos.pc import Pc
+from modelos.unidad_control import UnidadControl
+from modelos.unidad_control_cableada import UnidadControlCableada
 
- # Asegúrate de que el archivo esté accesible.
+# Asegúrate de que el archivo esté accesible.
 
 # Instancia de la aplicación Dash
 app = Dash(__name__)
@@ -21,9 +28,40 @@ monitor = DataMonitor()
 alu = ALU()
 psw = Psw()
 ir = Ir()
+pc = Pc()
 mar = Mar()
 mbr = Mbr()
-pc = Pc()
+bus_direcciones = BusDirecciones()
+bus_datos = BusDatos()
+bus_control = BusControl()
+memoria_instrucciones = MemoriaInstrucciones(128)
+memoria_datos = MemoriaDatos(128)
+
+mar.bus_direcciones = bus_direcciones
+mbr.bus_datos = bus_datos
+bus_datos.mbr = mbr
+memoria_instrucciones.bus_control = bus_control
+memoria_instrucciones.bus_direcciones = bus_direcciones
+memoria_instrucciones.bus_datos = bus_datos
+
+memoria_datos.bus_control = bus_control
+memoria_datos.bus_direcciones = bus_direcciones
+memoria_datos.bus_datos = bus_datos
+
+unidad_control = UnidadControl()
+unidad_control_cableada = UnidadControlCableada(
+    pc,
+    mar,
+    mbr,
+    bus_datos,
+    bus_direcciones,
+    bus_control,
+    memoria_instrucciones,
+    ir,
+    unidad_control,
+    psw,
+    alu
+)
 
 monitor.add_model("ALU", alu)
 monitor.add_model("PSW", psw)
@@ -31,6 +69,13 @@ monitor.add_model("IR",ir)
 monitor.add_model("MAR",mar)
 monitor.add_model("MBR",mbr)
 monitor.add_model("PC",pc)
+monitor.add_model("MEMORIAINSTRUCCIONES", memoria_instrucciones)
+monitor.add_model("MEMORIADATOS", memoria_datos)
+monitor.add_model("BUSDIRECCIONES", bus_direcciones)
+monitor.add_model("BUSDATOS", bus_datos)
+monitor.add_model("BUSCONTROL", bus_control)
+monitor.add_model("UNIDADCONTROL", unidad_control)
+monitor.add_model("UNIDADCONTROLCABLEADA", unidad_control_cableada)
 
 def start_monitoring():
     monitor.monitor_changes()
